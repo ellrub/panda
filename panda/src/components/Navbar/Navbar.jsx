e56ad0caf561c55c5adc5a3f88abbdbf949a95fd
ellrub <ellefsen88@gmail.com>
2022-10-16 18:12:55 +0200

    Material UI
    Added material UI buttons

--- panda/src/components/Navbar/Navbar.jsx
+++ panda/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,29 @@
-import React, {useState} from "react"
+import React from "react"
 import {AiOutlineHome, AiFillClockCircle, AiOutlineComment, AiOutlineEnvironment} from "react-icons/ai"
+import Button from '@mui/material/Button';
 
 import "./Navbar.scss";
 
+
 const Navbar = () => {
-    const [active, setActive] = useState(false)
+
+
     return (
-        <nav>
-            <a href=""><AiOutlineHome onClick={() => setActive(true)} /></a>
-            <a href=""><AiFillClockCircle onClick={() => setActive(true)} /></a>
-            <a href=""><AiOutlineEnvironment onClick={() => setActive(true)} /></a>
-            <a href=""><AiOutlineComment onClick={() => setActive(true)} /></a>
-        </nav>
+        <>
+            <nav>
+                <Button id="" className="navbar__button"><AiOutlineHome /></Button>
+                <Button id="time" className="navbar__button"><AiFillClockCircle /></Button>
+                <Button id="" className="navbar__button"><AiOutlineEnvironment /></Button>
+                <Button id="" className="navbar__button"><AiOutlineComment /></Button>
+            </nav>
+            <div className="navbar__modal-card--time">
+                <p>Tirsdag - Søndag</p>
+                <p>15.00 - 20.00</p>
+                <p>Mandag</p>
+                <p>Stengt</p>
+                <button className="modal-time--close">Lukk</button>
+            </div>
+        </>
     )
 }
 
