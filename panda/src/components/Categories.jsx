c3b9e84e2d4ab3accea2526cb48e518e2dc63155
ellrub <ellefsen88@gmail.com>
2022-10-18 12:41:17 +0200

    Styling
    Added styling to filter buttons
    Removed unnecessary code in index.css

--- panda/src/components/Categories.jsx
+++ panda/src/components/Categories.jsx
@@ -2,12 +2,14 @@ import React from "react"
 
 import "../App.scss"
 import "../index.css"
+import "./Categories.scss"
 
 const Categories = ({categories, filterItems, activeCategory}) => {
     return (
-        <div id="menu">
+        <div id="menu" className="categories__filter-btn--container">
             {categories.map((category, index) => {
                 return (
+                    
                     <button
                         type="button"
                         className={`${
