4724c4993109b526cb612fd02475387079491092
ellrub <ellefsen88@gmail.com>
2022-10-18 19:37:05 +0200

    Responsive

--- panda/src/container/Menu/Menu.jsx
+++ panda/src/container/Menu/Menu.jsx
@@ -4,33 +4,35 @@ import "./Menu.scss"
 
 const Menu = ({items}) => {
     return (
-        <main>
-            {items.map((item) => {
-                const {id, title, alergy, price} = item;
-                return (
-                    <article key={id}>
-                            <div className="menu__text-container">
-                                <h3 className="menu__text">{title}</h3>
-                                <h3 className="menu__text">{price}</h3>
-                            </div>
-                            <p className="menu__alergy">{alergy}</p>
-                            <div className="menu__underline-container">
-                                <div className="menu__underline"></div>
-                            </div>
-                    </article>
-                )
-            })}
-            <div className="menu__info">
-                <p>Alle våre kyllingretter kan også fås med vegetar kylling</p>
-                <h3>Alergener</h3>
-                <p>GL - Gluten | SO - Soya | SE - Sesam | PN - Peanøtt | E - Egg | N - Nøtter</p>
-                <p>Vi kan også lage de fleste retter glutenfri</p>
-            </div>
-            <div className="space">
+        <>
+            <main>
+                {items.map((item) => {
+                    const {id, title, alergy, price} = item;
+                    return (
+                        <article key={id}>
+                                <div className="menu__text-container">
+                                    <h3 className="menu__text">{title}</h3>
+                                    <h3 className="menu__text">{price}</h3>
+                                </div>
+                                <p className="menu__alergy">{alergy}</p>
+                                <div className="menu__underline-container">
+                                    <div className="menu__underline"></div>
+                                </div>
+                        </article>
+                    )
+                })}
 
-            </div>
+            </main>
+                <div className="menu__info">
+                    <p>Alle våre kyllingretter kan også fås med vegetar kylling</p>
+                    <h3>Alergener</h3>
+                    <p>GL - Gluten | SO - Soya | SE - Sesam | PN - Peanøtt | E - Egg | N - Nøtter</p>
+                    <p>Vi kan også lage de fleste retter glutenfri</p>
+                </div>
+                <div className="space">
 
-        </main>
+                </div>
+        </>
     )
 }
 
