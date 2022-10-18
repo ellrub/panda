e7b343badc46c831dcb277783c1e7610733ddc00
ellrub <ellefsen88@gmail.com>
2022-10-18 18:50:06 +0200

    Update header
    Added Social icons to header

--- panda/src/container/Header/Header.jsx
+++ panda/src/container/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {AiFillFacebook, AiFillInstagram} from "react-icons/ai"
 
 import {images} from "../../constants";
 import "./Header.scss";
@@ -19,6 +20,14 @@ const Header = () => {
                     className="header__container-link--menu header__link">
                         <i className="fa-solid fa-utensils"></i><br/>Meny
                 </a>
+                <div className="header__socialmedia-links">
+                    <a  rel="noreferrer" 
+                        href="https://www.facebook.com/pandamatsandsli/" 
+                        target="_blank"><AiFillFacebook /></a>
+                    <a  rel="noreferrer" 
+                        href="https://www.instagram.com/pandamatsandsli/" 
+                        target="_blank"><AiFillInstagram /></a>
+                </div>
             </div>
 
         </header>
